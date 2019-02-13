8d2b39d7c7d90f81f3473b7ca8691c9ed54da13e
LordComfy <comfyseven4@gmail.com>
2019-02-12 21:49:30 -0500

    + formatted print page to use napa label pages for printing (string coordinates) (8.5x11)

--- BarcodeGeneration/PrintPage.cs
+++ BarcodeGeneration/PrintPage.cs
@@ -42,45 +42,71 @@ namespace BarcodeGeneration
             }
         }
 
-        Bitmap memoryImage;
-
-        private void CaptureScreen()
-        {
-            Graphics myGraphics = this.CreateGraphics();
-            Size s = this.Size;
-            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
-            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
-        }
-
         //Print the document
         private void printToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.ControlBox = false;
-            this.Text = null;
-            
-            CaptureScreen();
-
-            Document doc = new Document();
+        {            
+            Document doc = new Document(PageSize.LETTER, 0, 0, 0, 0);
             try
             {
-                Image PrintForm;
-
-                using (Bitmap bmp = new Bitmap(this.Width, this.Height))
-                {
-                    this.DrawToBitmap(bmp, new System.Drawing.Rectangle(Point.Empty, bmp.Size));
-                    PrintForm = Image.GetInstance(bmp, System.Drawing.Imaging.ImageFormat.Png);
-                }
-
                 string pdfpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 iTextSharp.text.pdf.PdfWriter.GetInstance(doc, new FileStream(pdfpath + "/" + InfoPass.line.ToString() + " - " + InfoPass.partnumber.ToString() + ".pdf", FileMode.Create));
-                PrintForm.ScaleToFit(doc.PageSize.Width, doc.PageSize.Height);
-                doc.Open();
-                doc.Add(PrintForm);
-                doc.Close();
 
-                MessageBox.Show("PDF Document Created @ " + pdfpath + "/" + InfoPass.line.ToString() + " - " + InfoPass.partnumber.ToString() + ".pdf");
+                doc.Open();
+
+                //New barcode labels system per coordinates
+                Image Barcode = Image.GetInstance(InfoPass.Barcode, System.Drawing.Imaging.ImageFormat.Png);
+
+                Barcode.SetAbsolutePosition(65, 38); //Row 7 Label 1 (50, 220) * appeared at the bottom left of the document :? (65, 38) ?Wtf is this coord system
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 38); //Row 7 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 38); //Row 7 Label 3
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 147); //Row 6 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 147); //Row 6 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 147); //Row 6 Label 3 
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 255); //Row 5 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 255); //Row 5 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 255); //Row 5 Label 3 
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 365); //Row 4 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 365); //Row 4 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 365); //Row 4 Label 3 
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 474); //Row 3 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 474); //Row 3 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 474); //Row 3 Label 3 
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 583); //Row 2 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 583); //Row 2 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 583); //Row 2 Label 3 
+                doc.Add(Barcode);
+
+                Barcode.SetAbsolutePosition(65, 689); //Row 1 Label 1 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(260, 689); //Row 1 Label 2 
+                doc.Add(Barcode);
+                Barcode.SetAbsolutePosition(450, 689); //Row 1 Label 3 
+                doc.Add(Barcode);
+
+                doc.Close();
+                MessageBox.Show("PDF Document Created @ " + pdfpath + "/" + InfoPass.line.ToString() + " - " + InfoPass.partnumber.ToString() + ".pdf" + Environment.NewLine + "This document can be printed on the orange napa sticky labels.");
             }
             catch (Exception ex)
             {
