54b8c4e73522ab344390648121f0867d167a845d
Napa Workstation <napaWorkstation@zeropointgaming.net>
2019-02-15 10:05:19 -0500

    Fixed ui

--- BarcodeGeneration/Form1.Designer.cs
+++ BarcodeGeneration/Form1.Designer.cs
@@ -37,7 +37,6 @@
             this.UnformattedBarcodeTextbox = new System.Windows.Forms.TextBox();
             this.PartNumberTextbox = new System.Windows.Forms.TextBox();
             this.LineCodeTextbox = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -53,9 +52,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(10, 131);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 87);
+            this.pictureBox1.Size = new System.Drawing.Size(368, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -63,11 +62,11 @@
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Location = new System.Drawing.Point(196, 51);
+            this.button2.Location = new System.Drawing.Point(196, 33);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(182, 23);
             this.button2.TabIndex = 8;
-            this.button2.Text = "Generate And Copy";
+            this.button2.Text = "Generate Preview";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -81,7 +80,7 @@
             "UPC-A",
             "EAN-13",
             "ITF-14"});
-            this.comboBox1.Location = new System.Drawing.Point(92, 53);
+            this.comboBox1.Location = new System.Drawing.Point(92, 35);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(76, 21);
             this.comboBox1.TabIndex = 9;
@@ -93,7 +92,7 @@
             this.UnformattedBarcodeTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
             this.UnformattedBarcodeTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.UnformattedBarcodeTextbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.UnformattedBarcodeTextbox.Location = new System.Drawing.Point(174, 25);
+            this.UnformattedBarcodeTextbox.Location = new System.Drawing.Point(174, 7);
             this.UnformattedBarcodeTextbox.Name = "UnformattedBarcodeTextbox";
             this.UnformattedBarcodeTextbox.Size = new System.Drawing.Size(204, 20);
             this.UnformattedBarcodeTextbox.TabIndex = 16;
@@ -104,7 +103,7 @@
             this.PartNumberTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
             this.PartNumberTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.PartNumberTextbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.PartNumberTextbox.Location = new System.Drawing.Point(80, 105);
+            this.PartNumberTextbox.Location = new System.Drawing.Point(84, 88);
             this.PartNumberTextbox.Name = "PartNumberTextbox";
             this.PartNumberTextbox.Size = new System.Drawing.Size(84, 20);
             this.PartNumberTextbox.TabIndex = 19;
@@ -115,27 +114,16 @@
             this.LineCodeTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
             this.LineCodeTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.LineCodeTextbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LineCodeTextbox.Location = new System.Drawing.Point(67, 80);
+            this.LineCodeTextbox.Location = new System.Drawing.Point(73, 62);
             this.LineCodeTextbox.Name = "LineCodeTextbox";
             this.LineCodeTextbox.Size = new System.Drawing.Size(84, 20);
             this.LineCodeTextbox.TabIndex = 20;
             this.toolTip1.SetToolTip(this.LineCodeTextbox, "UPC-A QTY(1) MFG(51131) PN(26340) Chk(3)");
             // 
-            // button3
-            // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Location = new System.Drawing.Point(10, 22);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(158, 23);
-            this.button3.TabIndex = 15;
-            this.button3.Text = "Create Barcode Unformatted";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(193, 9);
+            this.label7.Location = new System.Drawing.Point(12, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(87, 13);
             this.label7.TabIndex = 17;
@@ -144,7 +132,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 56);
+            this.label4.Location = new System.Drawing.Point(12, 38);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 13);
             this.label4.TabIndex = 10;
@@ -153,9 +141,9 @@
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(196, 80);
+            this.button1.Location = new System.Drawing.Point(196, 58);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 45);
+            this.button1.Size = new System.Drawing.Size(182, 50);
             this.button1.TabIndex = 18;
             this.button1.Text = "Create Printable Document";
             this.button1.UseVisualStyleBackColor = true;
@@ -164,7 +152,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Location = new System.Drawing.Point(12, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 21;
@@ -173,7 +161,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 107);
+            this.label2.Location = new System.Drawing.Point(12, 90);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 22;
@@ -184,7 +172,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
-            this.ClientSize = new System.Drawing.Size(392, 230);
+            this.ClientSize = new System.Drawing.Size(392, 236);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.LineCodeTextbox);
@@ -192,7 +180,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.UnformattedBarcodeTextbox);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
@@ -212,7 +199,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox UnformattedBarcodeTextbox;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label4;
